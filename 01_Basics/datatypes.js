5d3bfb37ad92353c6892ccdffcf31616b3084ec1
Pavitra Pandey <154352488+2005pavitra@users.noreply.github.com>
2024-05-18 08:03:41 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,20 @@
+// "use strict"; //treat all js code as newer verdion
+
+// alert(3+3) //we are using node.js not browser
+
+console.log(3+3);
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol =>
+object
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
