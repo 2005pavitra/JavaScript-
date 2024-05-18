6787fbf859a7e77b3ed7a94e5e1c26ce65055ce6
Pavitra Pandey <154352488+2005pavitra@users.noreply.github.com>
2024-05-18 07:45:10 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 13241;
+let accountEmail = "pavitrapandeysagar";
+var accountPasword = "231413";
+accountCity = "bhopal";
+let accountState;
+
+// accountId = 2 //not allowed
+
+/*
+var is not used beacause of scope and functional limitations
+*/
+
+accountEmail = "sagar@gmail.com";
+accountPasword = "42387492";
+accountCity = "satna";
+
+console.table([accountId, accountPasword, accountState,accountCity,accountEmail])
