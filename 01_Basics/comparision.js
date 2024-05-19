c1d9e7e518b75fee0063a55d45616844722cd743
Pavitra Pandey <154352488+2005pavitra@users.noreply.github.com>
2024-05-19 11:39:10 +0000

    comparision

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02"> 1);
+// console.log("02"> 10);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null == undefined);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log("3" === 3);
