68ff98c685f45f220617b885477a70f4164d2cf3
Pavitra Pandey <154352488+2005pavitra@users.noreply.github.com>
2024-05-19 11:15:58 +0000

    conversion and operation

--- 01_Basics/conversionandoperarions.js
+++ 01_Basics/conversionandoperarions.js
@@ -1,1 +1,63 @@
-console.log("pavitra");
+// let score = "Pavitra";
+// let value = "33"
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof value);
+
+// let valueInNumber = Number(value);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let scoreInNumber = Number(score);
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// "Pavitra" => NaN
+// true => 1; false => 0 
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let someNumber = 12;
+
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ====================Operations=====================
+
+let value = 4;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(3-2);
+// console.log(3+2);
+// console.log(3*2);
+// console.log(3**3);
+// console.log(3/3);
+// console.log(3%3);
+
+let str1 = "hello";
+let str2 = "pavitra";
+
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1" + 2);
+console.log(2 + "2" + 3 + 4 + "4");
+console.log(3+4);
+console.log(3+4 + "4");
+
+let x = 100;
+x++;
+console.log(x);
+
+let y = 100;
+++y;
+console.log(y);
