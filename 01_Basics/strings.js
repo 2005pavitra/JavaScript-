24fe7302f2efffc7c190655fa7ba0e41fea9956f
Pavitra Pandey <154352488+2005pavitra@users.noreply.github.com>
2024-05-20 14:42:36 +0000

    strings

---
+++ 01_Basics/strings.js
@@ -0,0 +1,33 @@
+const name  = "Pavita";
+const repoCount = 10;
+
+// console.log(name + repoCount);
+
+// console.log(`hello, this is ${name} and  my repo count is ${repoCount}`);
+
+const gameName = new String('Pavitra- pandey');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(typeof gameName);
+// console.log(gameName.length);
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('t'));
+
+// console.log(gameName.substring(-2,4));
+// console.log(gameName.slice(-11, 4));
+// console.log(gameName.slice(2, 14));
+
+// console.log("The quick brown fox jumps over ".length);
+// console.log("The quick brown fox jumps over ".split(' '));
+// console.log("The quick brown fox jumps over ".includes('fox'));
+// console.log("  The quick brown fox jumps over    ".trim());
+// console.log("  The quick brown fox jumps over    ".replace('The', 'A'));
+
+const str1 = "Hello"
+const str2 = "world"
+
+console.log(str1.concat(str2));
+console.log(str1.concat(' ', str2));
+
+
