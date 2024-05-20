92c4967c4c509e627755b07e0c6c9ae80896f74e
Pavitra Pandey <154352488+2005pavitra@users.noreply.github.com>
2024-05-20 15:18:21 +0000

    numbers and maths

---
+++ 01_Basics/numbers_maths.js
@@ -0,0 +1,30 @@
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof balance);
+// console.log(balance.toString().length);
+
+const number = 342.893
+// console.log(number.toPrecision(4));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ========================= Maths =====================
+
+// console.log(Math);
+// console.log((Math.abs(-3)));
+// console.log(Math.round(3.5));
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(3.8));
+// console.log(Math.min(3,4,5,2));
+// console.log(Math.max(4,6,7,8));
+
+console.log((Math.random()));
+console.log((math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
